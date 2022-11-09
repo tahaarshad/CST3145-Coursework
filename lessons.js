bf00b07384b436322f7772c23d9aef4d84bf6922
taha.arshad <taha.arshad10@outlook.com>
2022-11-09 20:10:53 +0400

    include html, css, js and images
    Included index.html, style.css and lessons.js files along with images folder.
    Imported Vue and Font Awesome libraries as well.

---
+++ lessons.js
@@ -0,0 +1,73 @@
+let lessons = [
+    {
+    "id": 1,
+    "Subject": "Dance Class",
+    "Location": "Abu Hail",
+    "Price": 50,
+    "Space": 5,
+    "Image": "images/dance_class.png"
+    }, {
+    "id": 2,
+    "Subject": "Robotics (STEM)",
+    "Location": "Al-Twar",
+    "Price": 80,
+    "Space": 5,
+    "Image": "images/robotics_class.png"
+    }, {
+    "id": 3,
+    "Subject": "Football",
+    "Location": "Al-Qusais",
+    "Price": 50,
+    "Space": 5,
+    "Image": "images/football.png"
+    }, {
+    "id": 4,
+    "Subject": "Programming",
+    "Location": "Al-Twar",
+    "Price": 40,
+    "Space": 5,
+    "Image": "images/programming.png"
+    }, {
+    "id": 5,
+    "Subject": "Art Club",
+    "Location": "DIAC",
+    "Price": 30,
+    "Space": 5,
+    "Image": "images/art_club.png"
+    }, {
+    "id": 6,
+    "Subject": "Swimming",
+    "Location": "DIAC",
+    "Price": 60,
+    "Space": 5,
+    "Image": "images/swimming.png"
+    }, {
+    "id": 7,
+    "Subject": "Quran Club",
+    "Location": "Al-Twar",
+    "Price": 20,
+    "Space": 5,
+    "Image": "images/quran_club.png"
+    }, {
+    "id": 8,
+    "Subject": "Lego Builders",
+    "Location": "Al-Qusais",
+    "Price": 40,
+    "Space": 5,
+    "Image": "images/lego_builders.png"
+    }, {
+    "id": 9,
+    "Subject": "Public Speaking",
+    "Location": "Abu Hail",
+    "Price": 30,
+    "Space": 5,
+    "Image": "images/public_speaking.png"
+    }, {
+    "id": 10,
+    "Subject": "Children City Trip",
+    "Location": "Oud-Metha",
+    "Price": 50,
+    "Space": 5,
+    "Image": "images/school_trip.png"
+    }
+];
